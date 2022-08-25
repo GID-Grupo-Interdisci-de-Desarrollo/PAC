a97ac5e79ee12fd08f8906e6a9b65deb54fe1e57
JisusTQ <jesus.torresq@udea.edu.co>
2022-08-24 22:25:41 -0500

    Avance MovimientoDinero

--- PAC/src/gid/data/com/MovimientoDinero.java
+++ PAC/src/gid/data/com/MovimientoDinero.java
@@ -3,5 +3,36 @@ package gid.data.com;
 public class MovimientoDinero {
 
     private double monto;
+    String Concepto;
+    String Usuario;
 
+    public MovimientoDinero(double monto, String Concepto, String Usuario) {
+        this.monto=monto;
+        this.Concepto=Concepto;
+        this.Usuario=Usuario;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return Concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        Concepto = concepto;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
 }
